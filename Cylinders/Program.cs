b6543c89971c6fc8952c118001cfbd78ee663e72
Andrew Edson <docedson@gmail.com>
2019-04-09 13:31:31 -0500

    JustAddingNotes

--- Cylinders/Program.cs
+++ Cylinders/Program.cs
@@ -10,21 +10,27 @@ namespace Cylinders
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Let's solve a cylinder problem out!");
+            //Intro message to say what you're doing
+            Console.WriteLine("Let's use a cylinder calculator!");
+
+            //Getting height information from the user
             Console.WriteLine("Please enter the height you would like: ");
             string HeightAsString = Console.ReadLine();
             double height = Convert.ToDouble(HeightAsString);
             Console.WriteLine(" ");
 
+            //Getting the radius information from the user
             Console.WriteLine("Please enter the radius you would like: ");
             string RadiusAsString = Console.ReadLine();
             double radius = Convert.ToDouble(RadiusAsString);
             Console.WriteLine(" ");
 
+            //Giving the program a set variable and math from the user's input
             double pi = 3.1415926;
             double Volume = pi * radius * radius * height;
             double SA = 2*pi * radius * (radius + height);
 
+            //Spitting out the results
             Console.WriteLine("The volume is: " +Volume);
             Console.WriteLine("The surface area is: " +SA);
             Console.ReadKey();
